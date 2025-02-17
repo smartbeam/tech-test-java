fa077dc4892d8f7ea3302a6d8f64af018ce56f3c
vcanuel <vincent.canuel@idcapture.fr>
2025-02-17 11:00:40 +0100

    add Person mapping

--- src/main/java/co/smartbeam/csvparser/CSVParserController.java
+++ src/main/java/co/smartbeam/csvparser/CSVParserController.java
@@ -1,4 +1,26 @@
 package co.smartbeam.csvparser;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
 public class CSVParserController {
-}
+
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadCSV(@RequestParam("file") MultipartFile file) {
+        //TO COMPLETE
+        return null;
+    }
+
+    @GetMapping("/persons")
+    public List<Person> getAllPersons() {
+        return null;
+    }
+
+
+    }
--- src/main/java/co/smartbeam/csvparser/Person.java
+++ src/main/java/co/smartbeam/csvparser/Person.java
@@ -1,4 +1,18 @@
 package co.smartbeam.csvparser;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private int age;
+    private String email;
+    // Getters and Setters
+
 }

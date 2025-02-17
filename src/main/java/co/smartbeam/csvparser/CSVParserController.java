46a47b6cc5a952afc741fd2b9e572b3f0eb90909
vcanuel <vincent.canuel@idcapture.fr>
2025-02-17 10:51:23 +0100

    Initial commit

---
+++ src/main/java/co/smartbeam/csvparser/CSVParserController.java
@@ -0,0 +1,4 @@
+package co.smartbeam.csvparser;
+
+public class CSVParserController {
+}
---
+++ src/test/java/co/smartbeam/csvparser/CsvParserApplicationTests.java
@@ -0,0 +1,13 @@
+package co.smartbeam.csvparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvParserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

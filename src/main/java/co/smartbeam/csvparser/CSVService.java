efcb3cc1b67157dbbba3f48170d00fc8faa999de
vcanuel <vincent.canuel@idcapture.fr>
2025-02-17 11:27:39 +0100

    add todo + add test request

--- src/main/java/co/smartbeam/csvparser/CSVService.java
+++ src/main/java/co/smartbeam/csvparser/CSVService.java
@@ -2,13 +2,15 @@ package co.smartbeam.csvparser;
 
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+import org.springframework.stereotype.Service;
 
 import java.io.*;
 
+@Service
 public class CSVService {
 
 
-    public Boolean readCsv(String path) throws IOException {
+    public Boolean readCsv(String filePath) throws IOException {
         //TODO get file from path and convert
         InputStream is = null;
         try (Reader reader = new BufferedReader(new InputStreamReader(is))) {
@@ -16,6 +18,7 @@ public class CSVService {
                     .withType(Person.class)
                     .withIgnoreLeadingWhiteSpace(true)
                     .build();
+            //TODO save to DB
         }
         return true;
     }

3ad5ee4ed5643a67c6bb8effe231ab4c7641c73d
vcanuel <vincent.canuel@idcapture.fr>
2025-02-17 11:38:13 +0100

    improve todo for controller

--- src/main/java/co/smartbeam/csvparser/CSVParserController.java
+++ src/main/java/co/smartbeam/csvparser/CSVParserController.java
@@ -3,17 +3,17 @@ package co.smartbeam.csvparser;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.util.List;
 
-@RestController
+
 public class CSVParserController {
 
     @PostMapping("/upload")
-    public ResponseEntity<String> uploadCSV() throws IOException {
-        //TO COMPLETE
+    public ResponseEntity<Integer> uploadCSV() throws IOException {
+        //Todo save all persons from resources/person.csv
+        // Returns the number of persons
         return null;
     }
 
@@ -22,5 +22,7 @@ public class CSVParserController {
         //TODO : return list of persons
         return null;
     }
+
+    //TODO : return person by its Id
 
 }
